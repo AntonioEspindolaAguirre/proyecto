10db7723ba9abf512cf49147d9e8d7af7d2877d2
AntonioEspindolaAguirre <114432837+AntonioEspindolaAguirre@users.noreply.github.com>
2022-09-29 23:32:23 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>3520110524</title>
+	<link rel="stylesheet" type="text/css" href="css\style.css">
+	<link rel="stylesheet" type="text/css" href="fontello.css">
+	<script src="https://kit.fontawesome.com/974fbfdc76.js" crossorigin="anonymous"></script>
+
+
+</head>
+<body color="red">
+
+<h1 >Antonio Espindola Aguirre</h1>
+
+<h2 ><b> Curriculum vitae</h2>
+	<img src="img/img1.jpeg" >
+
+	<h3 >Sobre mi</h3>
+	<p >  Me llamo Antonio Espindola Aguirre actualmente soy un  estudiante  de la  carrela tecnologias de la  informacion en el area  del desarollo de software. Estoy cursando el tercer cuatrimestre, las materias que empeño desarollo de aplicaciones web, y base de datos para aplicaciones.</p>
+		<p></p>
+
+        
+
+		<table >
+            <tr>
+              <td><i class="fas fa-phone-alt"></i></i></td>
+              <td>2652114</td>
+            </tr>
+            <tr>
+              <td><i class="fas fa-envelope"></i></td>
+              <td>toño123@gmail.com</td>
+            </tr>
+            <tr>
+                <td><i class="fas fa-map-marker-alt"></i></td>
+                <td> s/n coronango</td>
+              </tr>
+              
+          </table><br><br>
+
+          <h4 class="h4">educacion</h4>
+
+
+  <table>
+
+  	<tr>
+  		<td>TSU</td>
+  		<td>Univercidad Tecnologica de huejotzingo</td>
+
+  	</tr>
+  	<tr>
+  		<td>Bachiller</td>
+  		
+  	    <td>bachillerato oficial benito juarez</td>
+  		
+  	</tr>
+
+
+  </table>
+
+<h5 class="h5">conocimientos y hablilidades</h5>
+  	<ol>
+  		
+  		
+        <li>base de datos</li>
+        <li>base de datos </li>
+        <li>interconeccon de redes</li>
+        <li>programacion</li>
+
+
+
+  	</ol><br>
+
+  <h6>Intereces</h6> 
+
+
+<a href="familia.html." class="a"  > Mi Familia </a><br>
+
+
+
+<br><br>
+
+<a href="https://www.facebook.com/tonny.aear.37" class="icon"><i class="fab fa-facebook-f"></i>
+</a>
+
+
+<a href="https://www.instagram.com/?hl=es-la#reactivated" class="icon2"><i class="fab fa-instagram"></i></a>
+
+
+
+
+
+
+
+
+
+	
+</body>
+</html>
